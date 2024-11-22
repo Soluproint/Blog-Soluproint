295f29845a22cfadd9ec2d7108783bcb3bf50c16
ManuelaLopezV <manuela.lopez@seguridadalamano.com>
2024-11-22 11:05:42 -0500

    Correcciones del Space para el Blog

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,21 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "local",
+      provider: "@strapi/provider-upload-aws-s3",
       providerOptions: {
-        sizeLimit: 1000000, // Tamaño límite en bytes
+        accessKeyId: env("DO_SPACES_ACCESS_KEY"), // Clave de acceso
+        secretAccessKey: env("DO_SPACES_SECRET_KEY"), // Clave secreta
+        region: "nyc3", // Región del Space
+        params: {
+          Bucket: env("DO_SPACES_BUCKET"), // Nombre del Space
+        },
+        endpoint: env("DO_SPACES_ENDPOINT"), // Endpoint del Space
       },
       actionOptions: {
-        upload: {},
-        delete: {},
+        upload: {
+          sizeLimit: 10000000, // Tamaño máximo del archivo 10MB
+        }, // Opciones para la acción de carga (normalmente vacío).
+        delete: {}, // Opciones para la acción de eliminación (normalmente vacío).
       },
     },
   },
