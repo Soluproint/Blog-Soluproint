2a0c7220dba466ef656f1a1a5bf81d42359821c8
ManuelaLopezV <manuela.lopez@seguridadalamano.com>
2024-11-29 11:17:21 -0500

    Configuración del Space

--- config/plugins.ts
+++ config/plugins.ts
@@ -23,25 +23,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   upload: {
-//     config: {
-//       provider: "aws-s3",
-//       providerOptions: {
-//         accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-//         secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-//         endpoint: env("DO_SPACE_ENDPOINT"),
-//         region: env("DO_SPACE_REGION"),
-//         params: {
-//           Bucket: env("DO_SPACE_BUCKET"),
-//         },
-//       },
-//       actionOptions: {
-//         upload: {},
-//         uploadStream: {},
-//         delete: {},
-//       },
-//     },
-//   },
-// });
